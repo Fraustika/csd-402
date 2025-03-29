f82f03ef2b0c19c97f9b9566f24368c22cc0838c
Deana_Akimov <watterrinbottle@gmail.com>
2025-03-29 15:42:53 -0700

    Module 03

---
+++ module-3/JavaApplication3/src/NestedLoops.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        int rows = 7; // Total number of rows
+
+        for (int i = 0; i < rows; i++) {
+            // Print leading spaces
+            for (int j = 0; j < rows - i - 1; j++) {
+                System.out.print("  ");
+            }
+
+            // Print the left half of the pattern
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num *= 2; // Double the number for the next position
+            }
+
+            // Print the right half of the pattern
+            num /= 4; // Adjust to start from the second half
+            for (int j = 0; j < i; j++) {
+                System.out.print(num + " ");
+                num /= 2; // Halve the number for the next position
+            }
+
+            // Print the "@" symbol aligned to the right
+            for (int j = 0; j < rows - i - 1; j++) {
+                System.out.print("  ");
+            }
+            System.out.println("@");
+        }
+    }
+}
